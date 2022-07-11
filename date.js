c6058cdd847f3e669f31b1c6cfb31b02a39f85f1
Vatsalya Roy <79377087+Vatsalya001@users.noreply.github.com>
2022-07-11 22:37:43 +0530

    Add files via upload

---
+++ date.js
@@ -0,0 +1,24 @@
+//jshint esversion:6
+module.exports.getDate = getDate;
+function getDate(){
+  let today = new Date();
+  let day = "";
+  let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+  day = today.toLocaleDateString("en-US", options);
+  return day;
+}
+
+module.exports.getDay = getDay;
+ function getDay(){
+   let today = new Date();
+   let day = "";
+   let options = {
+     weekday: "long",
+   };
+   day = today.toLocaleDateString("en-US", options);
+   return day;
+ }
